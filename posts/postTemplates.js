b00c7dee91c416ab0243b5baa035104a5f8108b3
Matt Policastro <matt@mattpolicastro.com>
2015-06-04 16:20:38 -0400

    Added Moment.js to fix date problems.

--- posts/postTemplates.js
+++ posts/postTemplates.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var handlebars = require('handlebars');
 var layouts = require('handlebars-layouts');
 var marked = require('marked');
+var moment = require('moment');
 
 handlebars.registerHelper(layouts(handlebars));
 handlebars.registerPartial('layout', fs.readFileSync(__dirname + '/layout.hbs', 'utf8'));
@@ -14,9 +15,8 @@ handlebars.registerHelper('marked', function(string) {
 handlebars.registerHelper('niceDate',function(date) {
 	if(!date) return '';
 	//var niceDate = date.toLocaleString('en-US', {year: 'numeric', month: 'long', day: 'numeric'});
-	var niceDate = new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'long', day: 'numeric'}).format(date);
-	console.log(niceDate);
-	return niceDate;
+	var momentDate = moment(date);
+	return momentDate.format('MMM Do, YYYY');
 });
 
 
