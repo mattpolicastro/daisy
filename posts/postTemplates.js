75b8e6d13b3279ff9630b526070b5e0ea21129c7
Matt Policastro <matt@mattpolicastro.com>
2015-06-04 16:14:07 -0400

    More date shenanigans

--- posts/postTemplates.js
+++ posts/postTemplates.js
@@ -13,7 +13,8 @@ handlebars.registerHelper('marked', function(string) {
 });
 handlebars.registerHelper('niceDate',function(date) {
 	if(!date) return '';
-	var niceDate = date.toLocaleString('en-US', {year: 'numeric', month: 'long', day: 'numeric'});
+	//var niceDate = date.toLocaleString('en-US', {year: 'numeric', month: 'long', day: 'numeric'});
+	var niceDate = new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'long', day: 'numeric'}).format(date);
 	console.log(niceDate);
 	return niceDate;
 });
