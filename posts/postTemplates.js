66aff842c11d7fef279f79d01f275cb752fb1561
Matt Policastro <matt@mattpolicastro.com>
2015-06-04 15:23:24 -0400

    Added a marked helper and handled case where document field was null.

--- posts/postTemplates.js
+++ posts/postTemplates.js
@@ -1,9 +1,16 @@
 var fs = require('fs');
 var handlebars = require('handlebars');
 var layouts = require('handlebars-layouts');
+var marked = require('marked');
 
 handlebars.registerHelper(layouts(handlebars));
 handlebars.registerPartial('layout', fs.readFileSync(__dirname + '/layout.hbs', 'utf8'));
+
+handlebars.registerHelper('marked', function(string) {
+	if(!string) return '';
+	var safeString = marked(string);
+	return new handlebars.SafeString(safeString);
+});
 
 function loadTemplate(name) {
 	return fs.readFileSync(__dirname + '/views/' + name, 'utf8');
