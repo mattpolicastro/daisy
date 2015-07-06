cc6c2b3a0c4c4c8cabb26829fe0636473be33daf
Matt Policastro <matt@mattpolicastro.com>
2015-07-06 11:01:14 -0400

    Re-named RSS feed and description to be less goofy.

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -11,14 +11,14 @@ var url = String(process.env.MONGO_URL || 'mongodb://localhost:27017/dumdum/');
 router.use(express.static('public'));
 
 mongo.connect(url, function(err, db) {
-	
+
 	var posts = db.collection('posts');
 	var bios = db.collection('bios');
-	
+
 	router.get('/', function(req, res) {
 		res.redirect('/posts');
 	});
-	
+
 	router.get('/posts', function(req, res) {
 		posts.find().sort({published: -1}).toArray(function(err, posts) {
 			if(posts != null) {
@@ -28,7 +28,7 @@ mongo.connect(url, function(err, db) {
 			}
 		});
 	});
-	
+
 	router.get('/posts/:slug', function(req, res) {
 		posts.findOne({slug: req.params.slug}, {limit: 1}, function(err, post) {
 			if(post != null) {
@@ -38,18 +38,18 @@ mongo.connect(url, function(err, db) {
 			}
 		});
 	});
-	
+
 	router.get('/about', function(req, res) {
 		bios.findOne({}, function(err, bio) {
 			if(err) res.sendStatus(404);
 			res.send(template.postPage(bio));
 		});
 	});
-	
+
 	router.get('/feed.xml', function(req, res) {
 		var feed = new RSS({
-			title: "All posts from www.mattpolicastro.com",
-			description: "All posts, updates, and more from @mattpolicastro",
+			title: "Matt Policastro",
+			description: "All posts from @mattpolicastro.",
 			feed_url: "http://www.mattpolicastro.com/feed.xml",
 			site_url: "http://www.mattpolicastro.com",
 			language: "en",
@@ -66,16 +66,16 @@ mongo.connect(url, function(err, db) {
 		 			date: post.published
 				});
 			});
-			
+
 			res.type('application/rss+xml');
 			res.send(feed.xml({indent: true}));
 		});
 	});
-	
+
 	router.use(function(req, res){
 		res.send(template.fourOhFour());
 	});
-	
+
 });
 
 module.exports = router;
