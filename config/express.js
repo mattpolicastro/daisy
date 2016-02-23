90a4d145705e783a83a6cf74b5d1ff99a1a0645c
Matt Policastro <matt@mattpolicastro.com>
2016-02-22 23:59:46 -0500

    Ported over the date helpers and dropped the (apparently?) unused listDate helper

--- config/express.js
+++ config/express.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const marked = require('marked');
+const moment = require('moment');
 const handlebars = require('handlebars');
 
 module.exports = configExpress;
@@ -11,13 +12,24 @@ function configExpress(app) {
   // Add static dir of public files
   app.use(express.static('public'));
 
+  // Configure locale for moment's date formatting
+  moment.locale('en-us');
+
   // Add Handlebars as the view engine
   var hbs = exphbs.create({
     defaultLayout: 'main',
     helpers: {
       marked: function(string) {
-        if(!string) return '';
+        if (!string) { return '' };
         return new handlebars.SafeString(marked(string));
+      },
+      scriptDate: function(date) {
+        if (!date) { let date = Date.now(); }
+        return moment(date).format('MMM Do, YYYY')
+      },
+      roboDate: function(date) {
+        if (!date) { let date = Date.now(); }
+        return moment(date).format('YYYY-MM-DD HH:mm:ssZ');
       }
     }
   })
--- posts/postTemplates.js
+++ posts/postTemplates.js
@@ -4,26 +4,6 @@ var layouts = require('handlebars-layouts');
 var marked = require('marked');
 var moment = require('moment');
 
-moment.locale('en-us');
-
-handlebars.registerHelper('listDate', function(date) {
-	if(!date) return '';
-	var momentDate = moment(date);
-	return momentDate.format('MMM D, YYYY');
-});
-
-handlebars.registerHelper('scriptDate', function(date) {
-	if(!date) return '';
-	var momentDate = moment(date);
-	return momentDate.format('MMMM Do, YYYY');
-});
-
-handlebars.registerHelper('roboDate', function(date) {
-	if(!date) return '';
-	var momentDate = moment(date);
-	return momentDate.format('YYYY-MM-DD HH:mm:ssZ');
-});
-
 function loadTemplate(name) {
 	return fs.readFileSync(__dirname + '/views/' + name, 'utf8');
 };
