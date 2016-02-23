faa656477e70f9266a9fdcaffab3ac295a157829
Matt Policastro <matt@mattpolicastro.com>
2016-02-23 00:31:53 -0500

    Changed naming convention for files at a directory root and update the routers for those areas

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@
 const router = require('express').Router();
 
 router.get('/', function(req, res) {
-  res.render('home');
+  res.render('index');
 });
 
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -1,1 +1,9 @@
-module.exports = function(){};
+'use strict';
+
+const router = require('express').Router();
+
+router.get('/', function(req, res) {
+  res.render('posts/index');
+});
+
+module.exports = router;
