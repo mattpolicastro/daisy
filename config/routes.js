4ec9518b5757eb3e84233c15b18cfab023af04dc
Matt Policastro <matt@mattpolicastro.com>
2016-02-22 23:30:12 -0500

    Added a route for the 404 page

--- config/routes.js
+++ config/routes.js
@@ -7,4 +7,9 @@ function configRoutes(app) {
   // Load and mount handlers/routers
   app.use('/', require('../routes/index'));
   app.use('/posts', require('../routes/posts'));
+
+  // Send 404
+  app.use(function(req, res) {
+    res.status(404).render('404');
+  });
 }
