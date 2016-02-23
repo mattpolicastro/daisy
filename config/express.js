e72c50b1e7325bbf2f20cc69ef274a5837aebfbc
Matt Policastro <matt@mattpolicastro.com>
2016-02-22 23:04:50 -0500

    Actually added express-handlebars to the project

--- config/express.js
+++ config/express.js
@@ -1,10 +1,17 @@
 'use strict';
 
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 module.exports = configExpress;
 
 function configExpress(app) {
   // Add static dir of public files
   app.use(express.static('public'));
+
+  // Add Handlebars as the view engine
+  app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+  app.enable('view cache');
+  app.set('view engine', 'handlebars');
+
 }
