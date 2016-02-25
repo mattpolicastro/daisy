b32fadfba1df653e7e97799493ab1323fd9c6082
Matt Policastro <matt@mattpolicastro.com>
2016-02-25 18:44:43 -0500

    First pass at using mongoose model discriminators

--- models/blog.js
+++ models/blog.js
@@ -2,14 +2,34 @@
 
 const mongoose = require('mongoose');
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  makeDate: Date,
-  editDate: { type: Date, default: Date.now },
-  postDate: Date,
-  slug: { type: String, trim: true},
-  summary: String,
-  body: String
+const postSchema = new mongoose.Schema({
+  published: Date,
+  preview: String,
+  type: String,
+});
+const Post = new mongoose.model('Post', postSchema);
+
+const tweetSchema = new mongoose.Schema({
+  tweetUrl: {type: String, trim: true},
+  length: Number
 });
 
-module.exports = blogSchema;
+const blogSchema = new mongoose.Schema({
+  slug: { type: String, trim: true },
+  content: String,
+  /* Thinking about how to version posts over time; should all fields fall into
+   * an array of full posts, or should some things (like the post slug) stay
+   * constant?
+   * OR
+   * Keep the current version's info as top-level field; on changes, create a
+   * new document in an "Archive" collection with the now-outdated fields and
+   * insert the new doc's ObjectId into the versions array
+   */
+  versions: [{ type: Schema.Types.ObjectId, ref: 'BlogVersions'}]
+});
+
+module.exports = {
+  Post: new mongoose.model('Post', postSchema),
+  Blog: Post.discriminator('Blog', blogSchema),
+  Tweet: Post.discriminator('Tweet', tweetSchema)
+};
