7bf08bfc8495f67dc829a33289c311cb4f6aafe7
Matt Policastro <matt@mattpolicastro.com>
2016-08-12 16:37:15 -0400

    Updated styles and views

--- src/models/post.js
+++ src/models/post.js
@@ -1,13 +1,11 @@
 'use strict';
 
 module.exports = function(sequelize, DataTypes) {
-  let Post = sequelize.define('post', {
+  return sequelize.define('post', {
     status: DataTypes.ENUM('draft', 'published'),
     slug: DataTypes.STRING,
     summary: DataTypes.STRING,
     postType: DataTypes.ENUM('blog', 'tweet'),
     contents: DataTypes.JSONB
   });
-
-  return Post;
 };
