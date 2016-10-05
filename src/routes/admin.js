8c4ff944dc8b25ea4251c588826718b1bf70f075
Matt Policastro <matt@mattpolicastro.com>
2016-10-04 20:06:54 -0400

    First pass at profile settings

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let About = require('../models').Post.scope('abouts');
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
@@ -7,7 +9,40 @@ router.get('/', (req, res) => {
 });
 
 router.get('/settings', (req, res) => {
-  res.render('admin/settings');
+  About.findOne({
+    where: { UserId: req.user.id },
+    limit: 1,
+    order: [['createdAt', 'DESC']]
+  }).then((about) => {
+    res.render('admin/settings', {
+      title: 'Settings',
+      profile: about.contents.body
+    });
+  }).catch((err) => {
+    res.status(500).send(err);
+  });
 });
+
+router.post('/settings/profile', (req, res) => {
+  if (req.body) {
+    About.create({
+      status: 'published',
+      slug: Date.now().toString(),
+      summary: '',
+      type: 'about',
+      contents: {
+        body: req.body.about
+      },
+      UserId: req.user.id
+    }).then(() => {
+      req.flash('success', 'Profile successfully updated');
+      res.redirect('/admin/settings');
+    }).catch((err) => {
+      res.status(500).send(err);
+    });
+  }
+});
+
+
 
 module.exports = router;
